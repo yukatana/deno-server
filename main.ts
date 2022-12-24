8d64197f0b09712ba25408b49bd1ffc6ce6d145a
yukatana <rommel.aranguren@gmail.com>
2022-12-23 23:36:47 -0300

    fininshed dynamic HTML implementation

--- main.ts
+++ main.ts
@@ -1,17 +1,43 @@
-import { Application, Router } from './deps/deps'
+import { Application, Router } from './deps/deps.ts'
 
-let colors: string[]
+const htmlStart: string = `
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+    <meta charset='UTF-8'>
+    <title>Title</title>
+    </head>
+    <body style='background-color: black'>
+    <form action='http://localhost:8080' method='post'>
+        <input type='text' name='color' id='colorInput' value='Add a color to the list!'/>
+        <input type='submit' value='Submit'>
+    </form>
+    <ul>`
+
+let dynamicHtml: string = ''
+const addColorToHtml = (color) => {
+    return dynamicHtml = dynamicHtml.concat(`<li style='color: ${color}'>${color}</li>`)
+}
+
+let htmlEnd: string = `</ul></body></html>`
+
+let colors: string[] = []
 
 const router = new Router()
 router
     .get('/', (ctx) => {
-      ctx.response.headers.set('Content-Type', 'text/html')
-      ctx.response.body = ``
+        const HTML = htmlStart.concat(dynamicHtml, htmlEnd)
+        ctx.response.headers.set('Content-Type', 'text/html')
+        ctx.response.body = HTML
     })
-    .post('/', (ctx) => {
-      const { color } = ctx.request.body
-      colors.push(color)
-      ctx.response.body = `Color ${color} has been successfully added.`
+    .post('/', async (ctx) => {
+        const color = await ctx.request.body({type: 'form'}).value
+        const fixedColor = color.get('color')
+        colors.push(fixedColor)
+        addColorToHtml(fixedColor)
+        const HTML = htmlStart.concat(dynamicHtml, htmlEnd)
+        ctx.response.headers.set('Content-Type', 'text/html')
+        ctx.response.body = HTML
     })
 
 const app = new Application()
